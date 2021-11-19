424435bb7f4b77012cf60210805f36e7e2c49062
vamshi408 <bandi.vamshi408@gmail.com>
2021-11-19 11:56:52 +0530

    testing by vamshi for un

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestoneComponent } from './testone/testone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestoneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/testone/testone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testone',
+  templateUrl: './testone.component.html',
+  styleUrls: ['./testone.component.css']
+})
+export class TestoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
